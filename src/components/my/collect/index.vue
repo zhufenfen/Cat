df716778a50c3ee5fdeaf109f6d3058cce6ae5ed
hurenjie <1596910974@qq.com>
2019-01-07 14:18:38 +0800

    hurenjie

---
+++ src/components/my/collect/index.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <Collect-com/>
+    </div>
+</template>
+
+<script>
+import Collect from "./components/collect"
+export default {
+    components:{
+             "Collect-com":Collect,
+    }
+}
+</script>
+
+
+<style lang="sass">
+    
+</style>
---
+++ src/components/my/pet/index.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <Pet-com/>
+    </div>
+</template>
+
+<script>
+import Pet from "./components/pet"
+export default {
+    components:{
+             "Pet-com":Pet,
+    }
+}
+</script>
+
+
+<style lang="sass">
+    
+</style>
