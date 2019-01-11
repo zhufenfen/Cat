9ff235255f10cc6877e77331f41aafca3799b6d1
1 <446970480@qqcom>
2019-01-11 09:23:42 +0800

    提交我的页面

--- src/components/my/components/fans/component/header.vue
+++ src/components/my/components/fans/component/header.vue
@@ -1,21 +1,33 @@
 <template>
   <div>
     <div class="header">
+      <div class="lt" @click="getBack()"><img src="static/myImgs/left.png"></div>
       <p>关注</p>
       <p>粉丝</p>
+      <span> </span>
     </div>
   </div>
 </template>
 <script>
-export default {};
+export default {
+  methods: {
+    getBack(){
+      this.$router.back();
+    }
+  },
+};
 </script>
 <style lang="scss" scoped>
 .header {
   width: 100%;
   height: 1.27rem;
   display: flex;
-  justify-content: space-around;
+  justify-content: space-between;
   align-items:center;
+  .lt{
+    margin-left:.51rem;
+  }
+
   p {
     font-size: 36px;
     font-family: PingFang-SC-Regular;
---
+++ src/components/my/components/it/index.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="my">
+        <Header-com></Header-com>
+        <Main-com></Main-com>
+    </div>
+</template>
+
+<script>
+import Header from './components/header';
+import Main from './components/main';
+export default {
+    components:{
+        "Header-com":Header,
+        "Main-com":Main
+    }
+}
+    
+</script>
+
+
+<style lang="scss" scoped>
+    .my{
+        width:100%;
+        height:100%;
+        display:flex;
+        flex-direction: column;
+        background:rgba(241,241,241,1);
+    }
+</style>
