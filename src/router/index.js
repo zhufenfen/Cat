7fa39be3be0ce21eee8d4f1388658217a5222dea
1 <446970480@qqcom>
2019-01-11 13:37:34 +0800

    1-11-1

--- src/router/index.js
+++ src/router/index.js
@@ -16,14 +16,11 @@ import My from "@/components/my"                        //我的页面
 import Fans from "@/components/my/components/fans"      //跳转到粉丝
 import It from "@/components/my/components/it"          //跳转到他人主页
 import Dell from "@/components/my/components/dell"      //跳转到动态
-<<<<<<< HEAD
 import Not from "@/components/my/components/not"        //跳转到无数据页面
-=======
 
 /* ----------商城路由----------- */
 import ShopList from "@/components/shop/shopList"
 import GoodDetail from "@/components/shop/goodDetail"
->>>>>>> master
 import Err from "@/components/error"
 import Login from "@/components/login"
 
@@ -55,31 +52,6 @@ var router = new Router({
     {
       path: "/",
       redirect: "/home"
-<<<<<<< HEAD
-    },
-    {
-      path: "/home",
-      name: "home",
-      component: Home,
-      meta: {
-        requireAuth: true,
-        flag: true
-      }
-    },
-    {
-      path: "/community",
-      name: "community",
-      component: Community,
-      meta: {
-        requireAuth: true,
-        flag: true
-      }
-    },
-    {
-      path: "/publish",   //发布路由
-      name: "publish",
-      component: Publish,
-=======
     },
     {
       path: "/home",
@@ -161,29 +133,15 @@ var router = new Router({
       path: "/post",   //发布页面跳转到发帖页面
       name: "post",
       component: Post,
->>>>>>> master
       meta: {
         requireAuth: true,
         flag: false
       }
     },
     {
-<<<<<<< HEAD
       path: "/post",   //发布页面跳转到发帖页面
       name: "post",
       component: Post,
-=======
-      path: "/photo",   //发布页面跳转到相册页面
-      name: "photo",
-      component: Photo,
->>>>>>> master
-      meta: {
-        requireAuth: true,
-        flag: false
-      }
-    },
-    {
-<<<<<<< HEAD
       path: "/photo",   //发布页面跳转到相册页面
       name: "photo",
       component: Photo,
@@ -193,21 +151,6 @@ var router = new Router({
       }
     },
     {
-      path: "/dynamic",   //相册页面跳转到发布动态页面
-      name: "dynamic",
-      component: Dynamic,
-      meta: {
-        requireAuth: true
-      }
-    },
-    {
-      path: "/shop",
-      name: "shop",
-      component: Shop,
-      meta: {
-        requireAuth: true,
-        flag: true
-=======
       path: "/dynamic",   //相册页面跳转到发布动态页面
       name: "dynamic",
       component: Dynamic,
@@ -240,7 +183,6 @@ var router = new Router({
       meta:{
         requireAuth:false,
         flag:false
->>>>>>> master
       }
     },
     {
@@ -249,7 +191,6 @@ var router = new Router({
       component: Fans,
     },
     {
-<<<<<<< HEAD
       path: "/my/components/it",//粉丝页面跳转到他人主页
       name: "It",
       component: It,
@@ -273,15 +214,6 @@ var router = new Router({
         requireAuth: true,
         flag: true
       },
-=======
-      path: "/my/components/dell",//我的页面跳转到动态页面
-      name: "Dell",
-      component: Dell,
-      meta: {
-        requireAuth: true,
-        flag: false,
-      }
->>>>>>> master
     },
     {
       path: "/my/article",//my信息页面
@@ -291,13 +223,6 @@ var router = new Router({
         requireAuth: true
       },
     },
-<<<<<<< HEAD
-    {
-      path: "/my/collect",//my收藏页面
-      name: "collect",
-      component: Collect,
-      meta: {
-=======
         {
           path: "/my/article",
           name: "article",
@@ -408,30 +333,10 @@ var router = new Router({
       component: Details,
       meta: {
         flag: false,
->>>>>>> master
         requireAuth: true
       }
     },
     {
-<<<<<<< HEAD
-      path: "/my/pet",//my宠物界面
-      name: "pet",
-      component: Pet,
-      meta: {
-        requireAuth: true
-      }
-    },
-
-    {
-      path: "/login",
-      name: "login",
-      component: Login,
-    },
-    {
-      path: "**",
-      component: Err
-    }
-=======
       path: "/catDetail",
       name: "catDetail",
       component: CatDetail,
@@ -445,7 +350,6 @@ var router = new Router({
       component: Err
     },
 
->>>>>>> master
   ]
 })
 // 全局守卫（登录验证）
